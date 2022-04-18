1cfc2e943b2f0d582f666d4323cb1f9d9f7da5cb
avanish-n <91162961+avanish-n@users.noreply.github.com>
2022-04-18 11:48:04 -0400

    new addresses on assembly

---
+++ SCOMP_Project_spr22/Patternalt.asm
@@ -0,0 +1,21 @@
+org 0
+
+LOADI 10
+OUT PATTERN_ALT
+
+; IO address constants
+Switches:  EQU 000
+LEDs:      EQU 001
+Timer:     EQU 002
+Hex0:      EQU 004
+Hex1:      EQU 005
+
+PXL_A:     EQU &H0B0
+PXL_D:     EQU &H0B1
+
+PXL_ALL:   EQU &H0B2
+INC_REV:   EQU &H0B3
+PXL_24_RB:  EQU &H0B4
+PXL_24_G:   EQU &H0B5
+PATTERN_INC: EQU &H0B6
+PATTERN_ALT: EQU &H0B8
